02043a316f8dcfd51c58b5767910adc5e9d1efe7
zhekaf <a842002@gmail.com>
2019-11-25 21:37:25 +0200

    first commit
    full game

---
+++ Assets/MonsterMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterMovement : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    NavMeshPath path;
+    public float timeForNextPath;
+    bool inCoRoutine;
+    Vector3 target;
+    bool validPath;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoRoutine)
+        {
+            StartCoroutine(Wait());
+        }
+        
+    }
+
+    Vector3 randomPosition()
+    {
+        float pX = Random.Range(-20f, 20f);
+        float pZ = Random.Range(-20f, 20f);
+        Vector3 p = new Vector3(pX, 0f, pZ);
+        return p;
+    }
+
+    IEnumerator Wait()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timeForNextPath);
+        GetNewPath();
+        validPath = navMeshAgent.CalculatePath(target, path);
+        if (!validPath) Debug.Log("Found an invalid Path");
+        while (!validPath)
+        {
+            yield return new WaitForSeconds(0.01f);
+            GetNewPath();
+            validPath = navMeshAgent.CalculatePath(target, path);
+        }
+        inCoRoutine = false;
+    }
+
+    void GetNewPath()
+    {
+        target = randomPosition();
+        navMeshAgent.SetDestination(target);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    Transform cameraT;
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.2f;
+    CharacterController colider;
+    public float jumpHeight = 5.5f;
+    public float gravity = -12f;
+    float velocityY;
+
+    void Start()
+    {
+        cameraT = Camera.main.transform;
+        colider = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerController();
+    
+    }
+
+    void PlayerController()
+    {
+        Vector3 playerController = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Vector3 pControl = playerController.normalized;
+        if (pControl != Vector3.zero)
+        {
+            float targetRotation = Mathf.Atan2(pControl.x, pControl.z) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+        float speed = 10 * playerController.magnitude;
+        Vector3 velocity = transform.forward * speed + Vector3.up * velocityY;
+        colider.Move (velocity * Time.deltaTime);
+        velocityY += Time.deltaTime * gravity;
+        if (colider.isGrounded)
+        {
+            velocityY = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (colider.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+}
